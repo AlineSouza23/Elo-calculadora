356af851107a6158fba5e75cc9122f0510dd4293
Aline Souza <aline2308souza@gmail.com>
2025-03-30 22:53:28 -0300

    sem erro

--- src/router.tsx
+++ src/router.tsx
@@ -1,51 +1,54 @@
-import { createBrowserRouter, Navigate } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { ReactElement } from "react";
 import { Dash } from "./pages/app/dash";
 import { SECRET_KEY, SignIn, TOKEN_EXPIRATION_TIME } from "./pages/auth/sign-in";
 
-import CryptoJS from "crypto-js"
-import Cookies from 'js-cookie'
+import CryptoJS from "crypto-js";
+import Cookies from "js-cookie";
 
-//Componente para privar rotas com base no login do usuario
-const PrivateRoute = ({ element }) => {
-   return isAuthenticated() ? element : <Navigate to="/sign-in"/>
+// Tipagem para as propriedades do PrivateRoute
+interface PrivateRouteProps {
+  element: ReactElement;
 }
+
+// Componente para proteger rotas privadas
+const PrivateRoute = ({ element }: PrivateRouteProps) => {
+  return isAuthenticated() ? element : <Navigate to="/sign-in" />;
+};
 
 export const logout = () => {
-    Cookies.remove('authToken');
-    Cookies.remove('authHash');
+  Cookies.remove("authToken");
+  Cookies.remove("authHash");
 };
-  
 
-export const isAuthenticated  = () => {
-    const token = Cookies.get('authToken');
-    const hash = Cookies.get('authHash');
+export const isAuthenticated = (): boolean => {
+  const token = Cookies.get("authToken");
+  const hash = Cookies.get("authHash");
 
-    if (!token || !hash ) {
-        logout()
+  if (!token || !hash) {
+    logout();
+    return false;
+  }
 
-        return false
-    }
+  const validHash = CryptoJS.HmacSHA256(token, SECRET_KEY).toString();
+  if (hash !== validHash) return false;
 
-    const validHash = CryptoJS.HmacSHA256(token, SECRET_KEY).toString();
-    if (hash !== validHash) return false
+  const tokenParts = token.split(".");
+  const timestamp = parseInt(tokenParts[1], 10);
 
-    const tokenParts = token.split('.')
-    const timestamp = parseInt(tokenParts[1], 10)
+  if (Date.now() - timestamp > TOKEN_EXPIRATION_TIME) {
+    logout();
+    return false;
+  }
 
-    if(Date.now() - timestamp > TOKEN_EXPIRATION_TIME) {
-        logout()
-        return false
-    }
+  return hash === validHash;
+};
 
-    return hash === validHash
-}
-
+// Configuração das rotas
 export const router = createBrowserRouter([
-    { 
-        path: '/', 
-        element: <PrivateRoute element={<Dash />} />
-        
-    },
-    { path: '/sign-in', element: <SignIn />}
-])
-
+  {
+    path: "/",
+    element: <PrivateRoute element={<Dash />} />,
+  },
+  { path: "/sign-in", element: <SignIn /> },
+]);
